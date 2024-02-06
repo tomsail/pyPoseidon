c1a90d137159d86c514f1c57f183dfb9947d9050
tomsail <saillour.thomas@gmail.com>
2024-02-06 14:28:06 +0100

    [init] revert back d3d on + added Telemac

--- pyposeidon/__init__.py
+++ pyposeidon/__init__.py
@@ -3,8 +3,9 @@
 __author__ = "George Breyiannis (breyiannis@gmail.com)"
 __license__ = "EUPL-1.2"
 __version__ = "0.6.0"
-# from .d3d import d3d
+from .d3d import d3d
 from .schism import Schism
+from .telemac import Telemac
 
 from .tools import setup_logging
 
